393cc6ef9d42535f568c71594a9743870ca43f31
Jhones <jhonesaly@gmail.com>
2023-01-23 17:17:52 -0300

    Modifying the views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -10,7 +10,7 @@ def post_list(request):
         {'posts': posts}
         )
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     # try:
     #     post = Post.published.get(id=id)
 
@@ -19,8 +19,11 @@ def post_detail(request, id):
 
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
 
     return render(
