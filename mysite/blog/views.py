be9897bac760024f09482d481473fb11bf6c623a
Jhones <jhonesaly@gmail.com>
2023-01-26 19:40:46 -0300

    Building class-based views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator
 #from django.http import Http404
+from django.views.generic import ListView
+
 
 def post_list(request):
     posts = Post.published.all()
@@ -37,3 +39,12 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post': post}
     )
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
